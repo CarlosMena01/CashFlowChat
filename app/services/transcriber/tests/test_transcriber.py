3995b641687a1a2504bfad61303b5df47a376795
CarlosMena01 <csmena0223@gmail.com>
2025-07-20 21:14:31 -0500

    [DEV] Full transcriber with whisper

--- app/services/transcriber/tests/test_transcriber.py
+++ app/services/transcriber/tests/test_transcriber.py
@@ -9,9 +9,12 @@ sys.path.append(directorio_padre)
 
 from transcriber import Transcriber
 
-transcriber = Transcriber()
+transcriber = Transcriber("large")
 
-for i in range(0,6):
-    audio_file_path = f"tests/Test_0{i}.wav"  
-    resultado = transcriber.audio_to_text(audio_file_path)
-    print(f"Texto reconocido {i}:", resultado)
+# for i in range(0,6):
+#     audio_file_path = f"Test_0{i}.wav"  
+#     resultado = transcriber.audio_to_text(audio_file_path)
+#     print(f"Texto reconocido {i}:", resultado)
+
+texto = transcriber.audio_to_text("Test_04.wav")
+print("Transcripción:", texto)
